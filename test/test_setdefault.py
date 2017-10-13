48f7221c600db82a72525c20c0687686fc0080f8
Dustin Rohde <dustin.rohde@gmail.com>
2017-10-13 16:15:49 -0400

    More setdefault tests

--- test/test_setdefault.py
+++ test/test_setdefault.py
@@ -1,28 +1,71 @@
+from collections import OrderedDict, defaultdict
+from functools import partial
+
+import pytest
+
+from utils.merge import merge
 from utils.setdefault import setdefault
 
 
-class TestSetDefaultBasics:
+class SetDefaultTestBase:
+
+    func = setdefault
+
+    value = NotImplemented
+    default = NotImplemented
+    cls = None
+    other_cls = None
+
+    calculated_value = NotImplemented
 
     def test_both_values(self):
-        assert setdefault(5, 11) == 5
-        assert setdefault(11, 5) == 11
+        assert self.func(self.value, self.default) == self.calculated_value
 
-    def test_one_value(self):
-        assert setdefault(None, 5) == 5
-        assert setdefault(5, None) == 5
+    def test_none_value(self):
+        assert self.func(None, self.default) == self.default
 
-    def test_one_value_with_cls(self):
-        assert setdefault(None, 5, cls=str) == '5'
-        assert setdefault(5, None, cls=str) == '5'
+    def test_none_default(self):
+        assert self.func(self.value, None) == self.value
 
     def test_both_values_with_cls(self):
-        assert setdefault(5, 11, cls=str) == '5'
-        assert setdefault(11, 5, cls=str) == '11'
+        assert self.func(self.value, self.default, cls=self.cls) == \
+            self.cls(self.calculated_value)
 
-    def test_cls_param_none(self):
-        from collections import OrderedDict
-        value = OrderedDict((('a', 1), ('b', 2)))
-        assert type(setdefault(value, None)) == OrderedDict
-        assert type(setdefault(None, value)) == OrderedDict
+    def test_one_value_with_cls(self):
+        assert self.func(None, self.value, cls=self.cls) == \
+            self.cls(self.value)
+        assert self.func(self.value, None, cls=self.cls) == \
+            self.cls(self.value)
+
+    def test_one_value_with_non_builtin_cls(self):
+        value = self.other_cls()
+        assert type(self.func(value, None)) == self.other_cls
+        assert type(self.func(None, value)) == self.other_cls
 
 
+class TestSetDefaultSimple(SetDefaultTestBase):
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.func = setdefault
+
+        self.value = 5
+        self.default = 11
+        self.cls = str
+        self.other_cls = OrderedDict
+
+        self.calculated_value = self.value
+
+
+class TestSetDefaultMergeDicts:
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.func = partial(setdefault.merge_dicts, depth=-1)
+
+        self.value = {'a': 1, 'b': {'x': 9}}
+        self.default = {'a': 3, 'b': {'y': 8}, 'c': 5}
+        self.cls = tuple
+        self.other_cls = OrderedDict
+
+        self.calculated_value = merge(self.default, self.value)
