8406e69adb925a578f6ea94592e1f3f9fb1d6992
Dustin Rohde <dustin.rohde@gmail.com>
2017-10-18 05:25:00 -0400

    moretests

--- test/test_mappings.py
+++ test/test_mappings.py
@@ -1,37 +1,75 @@
+import copy
+
 import pytest
 
 from utils import mappings as maps
 
 
-def test_namespace():
-    ns = maps.Namespace((('a', 0), ('b', 1)), c=2)
-    assert ns.a == ns['a'] == 0
-    assert ns.b == ns['b'] == 1
-    assert ns.c == ns['c'] == 2
-    assert set(ns.items()) == set((('a', 0), ('b', 1), ('c', 2)))
-    ns.d = 3
-    ns.e = 4
-    assert ns.d == ns['d'] == 3
-    assert ns.e == ns['e'] == 4
-    with pytest.raises(KeyError):
-        ns.x
-    with pytest.raises(KeyError):
-        ns['x']
+class Namespace:
+
+    @pytest.fixture
+    def ns(self):
+        return maps.Namespace((('a', 0), ('b', 1)), c=2)
+
+    def test_create(self, ns):
+        assert ns.a == ns['a'] == 0
+        assert ns.b == ns['b'] == 1
+        assert ns.c == ns['c'] == 2
+        assert set(ns.items()) == set(pairs)
+
+    def test_update(self, ns):
+        ns.d = 3
+        ns.e = 4
+        assert ns.d == ns['d'] == 3
+        assert ns.e == ns['e'] == 4
+
+    def test_bad_lookups(self, ns):
+        with pytest.raises(KeyError):
+            ns.x
+        with pytest.raises(KeyError):
+            ns['x']
+
+    def test_copy(self, ns):
+        assert copy.copy(ns) == ns
+
+    def test_deepcopy(self):
+        ns = maps.Namespace((('a', 0), ('b', maps.Namespace('x', 9))), c=5)
+        assert copy.deepcopy(ns) == ns
 
 
-def test_frozendict():
-    items = (('a', 0), ('b', 1), ('c', 2))
-    non_items = (('z', 9), ('y', 8), ('x', 7))
-    dicts = (
-        maps.FrozenDict(items),
-        maps.FrozenDict(items[:1], **dict(items[1:])),
-        maps.FrozenDict(**dict(items)),
-    )
-    for d in dicts:
-        for k, v in items:
-            assert d[k] == v
-        for k, v in non_items:
-            with pytest.raises(TypeError):
-                d[k] = v
-            with pytest.raises(KeyError):
-                d[k]
+class TestFrozenDict:
+
+    @pytest.fixture
+    def items(self):
+        return (('a', 0), ('b', 1), ('c', 2))
+
+    @pytest.fixture
+    def non_items(self):
+        return (('z', 9), ('y', 8), ('x', 7))
+
+    @pytest.fixture
+    def dicts(self, items):
+        return (
+            maps.FrozenDict(items),
+            maps.FrozenDict(items[:1], **dict(items[1:])),
+            maps.FrozenDict(**dict(items)),
+        )
+
+    def test_existing_keys(self, dicts, items):
+        for d in dicts:
+            for k, v in items:
+                assert d[k] == v
+                with pytest.raises(TypeError):
+                    d[k] = 'abc'
+                with pytest.raises(TypeError):
+                    del d[k]
+
+    def test_non_existing_keys(self, dicts, non_items):
+        for d in dicts:
+            for k, v in non_items:
+                with pytest.raises(TypeError):
+                    d[k] = v
+                with pytest.raises(TypeError):
+                    del d[k]
+                with pytest.raises(KeyError):
+                    d[k]
