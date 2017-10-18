d23a629853147ee2972597bf7dd59b29ca73106f
Dustin Rohde <dustin.rohde@gmail.com>
2017-10-18 03:32:46 -0400

    more tests

--- test/test_seq.py
+++ test/test_seq.py
@@ -11,3 +11,17 @@ def test_tails():
     assert tuple(seq.tails([1, 2, 3])) == ([1, 2, 3], [1, 2], [1])
     assert tuple(seq.tails((5, 4, 9))) == ((5, 4, 9), (5, 4), (5,))
     assert tuple(seq.tails('abc')) == ('abc', 'ab', 'a')
+
+
+def test_cons():
+    assert hasattr(seq.cons(1, [2, 3]), '__next__')
+    assert tuple(seq.cons(1, [2, 3])) == (1, 2, 3)
+    assert list(seq.cons(1, (2, 3))) == [1, 2, 3]
+    assert list(seq.cons(1, iter([2, 3]))) == [1, 2, 3]
+
+
+def test_rcons():
+    assert hasattr(seq.rcons(1, [2, 3]), '__next__')
+    assert tuple(seq.rcons(1, [2, 3])) == (2, 3, 1)
+    assert list(seq.rcons(1, (2, 3))) == [2, 3, 1]
+    assert list(seq.rcons(1, iter([2, 3]))) == [2, 3, 1]
