3359bec89194525b82723e90eab753c87e544b77
Dustin Rohde <dustin.rohde@gmail.com>
2017-11-21 17:28:35 -0500

    Fixed some stuff up.

--- utils/nested.py
+++ utils/nested.py
@@ -12,8 +12,20 @@ OP_STARTS = OrderedDict((
 OP_ENDS = {
     Operation.KEY: [']'],
 }
+OP_CHARS = [
+    *OP_STARTS.keys(),
+    *(v for value in OP_ENDS.values() for v in value)
+]
 
 OPS_REPR = ' , '.join(f"'{char}'" for char in OP_STARTS.keys())
+
+
+class UnfinishedOperation(ValueError):
+
+    def __init__(self, op, char):
+        super().__init__(
+            f"unfinished `{op.name}` operation started with char '{char}';"
+            f" expected closing char '{OP_ENDS[op]}'")
 
 
 def _parse(path):
@@ -30,24 +42,26 @@ def _parse(path):
                 raise ValueError(
                     f"expected an operator ({OPS_REPR}) but got '{char}'")
         else:
-            if new_op or char in OP_ENDS.get(operation, ()):
-                instructions.append(Instruction(item, operation))
-                item = ''
-                if new_op:
-                    operation = new_op
-                else:
-                    operation = None
-            else:
+            if char not in OP_CHARS:
                 item += char
+                continue
+
+            instructions.append(Instruction(item, operation))
+            item = ''
+
+            if char in OP_ENDS.get(operation, ()):
+                if new_op:
+                    raise UnfinishedOperation(operation, char)
+                operation = None
+            elif new_op:
+                operation = new_op
 
     if operation is not None:
         if operation in OP_ENDS:
             for char, op in OP_STARTS.items():
                 if op is operation:
                     break
-            raise ValueError(
-                f"unfinished `{op.name}` operation started with char '{char}';"
-                f" expected closing char '{OP_ENDS[operation]}'")
+            raise UnfinishedOperation(op, char)
 
         instructions.append(Instruction(item, operation))
 
