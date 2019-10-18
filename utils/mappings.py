a3b1fc96a9f479f6df41a84c281d879195f858d6
Dustin Rohde <dustin.rohde@gmail.com>
2019-10-18 10:33:13 -0400

    progress on mappings

--- utils/mappings.py
+++ utils/mappings.py
@@ -1,18 +1,33 @@
-from collections.abc import Mapping
+from collections.abc import Mapping, MutableMapping, Set
 
 
 def subset(m0, m1):
     return {k: v for k, v in m0.items() if k in m1}
 
 
-class Namespace(dict):
-    """A dict that supports dot-access on its items."""
+class DictSet(dict, Set):
+    """A dict that also functions as a set over its keys."""
 
-    def __getattr__(self, attr):
-        return self.__getitem__(attr)
+    # def __init__(self, iterable=(), **kwargs):
+    #     self.__dict = dict(iterable, **kwargs)
 
-    def __setattr__(self, key, value):
-        return self.__setitem__(key, value)
+    # def __contains__(self, item):
+    #     return item in self.__dict
+
+    # def __iter__(self):
+    #     return iter(self.__dict)
+
+    # def __len__(self):
+    #     return len(self.__dict)
+
+    # def __getitem__(self, key):
+    #     return self.__dict[key]
+
+    # def __setitem__(self, key, value):
+    #     self.__dict[key] = value
+
+    # def __delitem__(self, key):
+    #     del self.__dict[key]
 
 
 class FrozenDict(Mapping):
@@ -29,3 +44,24 @@ class FrozenDict(Mapping):
 
     def __len__(self):
         return len(self.__dict)
+
+
+class FrozenDictSet(FrozenDict, Set):
+    """A ``FrozenDict`` that also functions as a set over its keys."""
+
+
+class _DotAccessItems:
+
+    def __getattr__(self, attr):
+        return self.__getitem__(attr)
+
+    def __setattr__(self, key, value):
+        return self.__setitem__(key, value)
+
+
+class Namespace(dict, _DotAccessItems):
+    """A dict that supports dot-access of its keys."""
+
+
+class FrozenNamespace(FrozenDict, _DotAccessItems):
+    """A ``FrozenDict`` that supports dot-access of its keys."""
