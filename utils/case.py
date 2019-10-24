61de85d33729841b36c0b6b820f35bcc290fa4bc
Dustin Rohde <dustin.rohde@gmail.com>
2019-10-24 15:52:15 -0400

    clean(case): add typing

--- utils/case.py
+++ utils/case.py
@@ -8,11 +8,11 @@ class CaseStyle(metaclass=abc.ABCMeta):
 
     @classmethod
     @abc.abstractmethod
-    def fmt(cls, words):
+    def fmt(cls, words: list[str]) -> str:
         pass
 
     @classmethod
-    def words(cls, string):
+    def words(cls, string: str) -> list[str]:
         return cls.WORD_PATTERN.findall(string)
 
 
@@ -21,7 +21,7 @@ class CamelCase(CaseStyle):
     WORD_PATTERN = re.compile(r'(?:\A[a-z_]|[A-Z_])[a-z0-9_]*')
 
     @classmethod
-    def fmt(cls, words):
+    def fmt(cls, words: list[str]) -> str:
         return words[0].lower() + ''.join(word.title() for word in words[1:])
 
 
@@ -30,7 +30,7 @@ class SnakeCase(CaseStyle):
     WORD_PATTERN = re.compile(r'(?:\A|_)(_*[A-Za-z0-9]+)')
 
     @classmethod
-    def fmt(cls, words):
+    def fmt(cls, words: list[str]) -> str:
         return '_'.join(word.lower() for word in words)
 
 
@@ -39,9 +39,9 @@ class KebabCase(CaseStyle):
     WORD_PATTERN = re.compile(r'(?:\A|-)(-*[A-Za-z0-9_]+)')
 
     @classmethod
-    def fmt(cls, words):
+    def fmt(cls, words: list[str]) -> str:
         return '-'.join(word.lower() for word in words)
 
 
-def convert(string, src, dest):
+def convert(string: str, src: CaseStyle, dest: CaseStyle):
     return dest.fmt(src.words(string))
