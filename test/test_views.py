9808ed5c685015192cd6c0e49641e76d21ec5fd2
Dustin Rohde <dustin.rohde@gmail.com>
2019-10-31 09:29:16 -0400

    test(views)

--- test/test_views.py
+++ test/test_views.py
@@ -6,11 +6,10 @@ from miscutils.views import DictView, SetView
 
 
 class TestDictView:
-
     @pytest.fixture
     def d(self):
         if not hasattr(self, "__d"):
-            self.__d = {0: "a", 1: "b", 2: "c", 3: "d"}
+            self.__d = {0: "a", 1: "b", 2: "c", "foo": "bar"}
         return self.__d
 
     @pytest.fixture
@@ -21,6 +20,11 @@ class TestDictView:
         assert copy.copy(dview) == dview
         assert copy.deepcopy(dview) == dview
 
+    def test_keys_values_items(self, dview):
+        assert set(dview.keys()) == {0, 2}
+        assert set(dview.values()) == {"a", "c"}
+        assert set(dview.items()) == {(0, "a"), (2, "c")}
+
     def test_get(self, d, dview):
         # OK - access items in view
         assert dview[0] == "a"
@@ -30,7 +34,7 @@ class TestDictView:
         with pytest.raises(KeyError):
             dview[1]
         with pytest.raises(KeyError):
-            dview[3]
+            dview["foo"]
         with pytest.raises(KeyError):
             dview[4]
 
@@ -46,8 +50,10 @@ class TestDictView:
     def test_set(self, d, dview):
         dview[0] = "A"
         assert d[0] == "A"
-        with pytest.raises(KeyError):
-            dview[1] == "B"
+        dview[1] = "B"
+        assert d[1] == "B"
+        dview[8] = "foo"
+        assert d[8] == "foo"
 
         d[2] = "C"
         assert dview[2] == "C"
@@ -60,11 +66,28 @@ class TestDictView:
         with pytest.raises(KeyError):
             dview[2]
 
+    def test_bulk_ops(self, d, dview):
+        dview.update(foo="biff")
+        assert d == {0: "a", 1: "b", 2: "c", "foo": "biff"}
+        dview.update(baz="quux")
+        assert d == {0: "a", 1: "b", 2: "c", "foo": "biff", "baz": "quux"}
+
+        dview.clear()
+        assert dview == {}
+        assert d == {1: "b"}
+
 
 class TestSetView:
+    @pytest.fixture
+    def s(self):
+        if not hasattr(self, "__s"):
+            self.__s = {"foo", "bar", "baz", "quux"}
+        return self.__s
 
-    def test_copy(self):
-        s = {"foo", "bar", "baz"}
-        view = SetView(s, ("foo", "baz"))
-        assert copy.copy(view) == view
-        assert copy.deepcopy(view) == view
+    @pytest.fixture
+    def sview(self, s):
+        return SetView(s, ("foo", "baz"))
+
+    def test_copy(self, sview):
+        assert copy.copy(sview) == sview
+        assert copy.deepcopy(sview) == sview
