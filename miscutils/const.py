8551e8555cfef3c5778a348527cc9d79d2ff6341
Dustin Rohde <dustin.rohde@gmail.com>
2019-11-07 17:24:04 -0500

    clean/doc(const)

--- miscutils/const.py
+++ miscutils/const.py
@@ -1,34 +1,42 @@
+"""namespaced constants"""
+
+
 class ConstError(TypeError):
-    pass
+    """Attempt to set or delete values of a Const."""
 
 
 class ConstMeta(type):
+    """Metaclass for Const."""
+
     def __new__(cls, name, bases, ns):
         # Extract defined constants
-        items = {
+        members = {
             key: val
             for key, val in ns.items()
             if key != "default_factory" and not key.startswith("__")
         }
-        for key in items:
+        for key in members:
             del ns[key]
 
         # Generate values for constants that are only named (e.g. FOO: str)
         types = ns.get("__annotations__", {})
         factory = ns.pop("default_factory", str.lower)
         for field_name in types:
-            if field_name not in items:
-                items[field_name] = factory(field_name)
+            if field_name not in members:
+                members[field_name] = factory(field_name)
 
-        ns["_items_"] = items
+        ns["__members__"] = members
 
         return super().__new__(cls, name, bases, ns)
 
     def __getitem__(cls, item):
-        return cls._items_[item]
+        return cls.__members__[item]
 
     def __getattr__(cls, attr):
-        return cls.__getitem__(attr)
+        try:
+            return cls.__getitem__(attr)
+        except KeyError as exc:
+            raise AttributeError(*exc.args)
 
     def __setattr__(cls, attr, value):
         raise ConstError("constants are read-only")
@@ -37,20 +45,23 @@ class ConstMeta(type):
         raise ConstError("constants are read-only")
 
     def __iter__(cls):
-        return iter(cls._items_)
+        return iter(cls.__members__)
 
     def __len__(cls):
-        return len(cls._items_)
+        return len(cls.__members__)
 
     def keys(cls):
-        return tuple(cls._items_.keys())
+        return tuple(cls.__members__.keys())
 
     def values(cls):
-        return tuple(cls._items_.values())
+        return tuple(cls.__members__.values())
 
     def items(cls):
-        return tuple(cls._items_.items())
+        return tuple(cls.__members__.items())
 
 
 class Const(metaclass=ConstMeta):
-    pass
+    """Immutable container for fixed values.
+
+    Derive from this class to define a group of constants.
+    """
